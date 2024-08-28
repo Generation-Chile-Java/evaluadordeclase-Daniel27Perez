f372521b748a09d92bbe181d6085d685bf25ba26
Daniel <dan.perezt@duocuc.cl>
2024-08-28 01:04:56 -0400

    evaluador notas v2

--- Evaluador.java
+++ Evaluador.java
@@ -6,39 +6,52 @@ public class Evaluador {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Ingrese las tres notas");
-
-
-        System.out.println("nota 1");
-        Integer nota1 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("nota 2");
-        Integer nota2 = sc.nextInt();
-        sc.nextLine();
-        System.out.println("nota 3");
-        Integer nota3 = sc.nextInt();
+        Integer nota1;
+        Integer nota2;
+        Integer nota3;
+        String resultado;
 
 
+            System.out.println("Ingrese las tres notas");
 
-            if ((nota1 < 0 && nota1 > 10) && (nota2 < 0 && nota2 > 10) && (nota3 < 0 && nota3 > 10)) {
-                System.out.println("Nota incorrecta");
-            } else if (((nota1 + nota2 + nota3) / 3) <= 3 ) {
-                System.out.println("No aprovado");
+            do{
+                System.out.println("nota 1");
+                nota1 = sc.nextInt();
+                if (nota1 < 0 || nota1 > 10){
+                    System.out.println("Nota inválida. Ingrese una nota entre 0 y 10.");
+
+                }
+            }while(nota1 < 0 || nota1 > 10);
+
+        do {
+            System.out.println("nota 2");
+            nota2 = sc.nextInt();
+            if (nota2 < 0 || nota2 > 10) {
+                System.out.println("Nota inválida. Ingrese una nota entre 0 y 10.");
             }
-            else if (((nota1 + nota2 + nota3) / 3) > 3 && ((nota1 + nota2 + nota3) / 3) <=5) {
-                System.out.println("Insuficiente");
-                
-            } else if (((nota1 + nota2 + nota3) / 3) > 5 && ((nota1 + nota2 + nota3) / 3) <=8) {
-                System.out.println("Aceptable");
-                
-            } else if (((nota1 + nota2 + nota3) / 3) > 8 && ((nota1 + nota2 + nota3) / 3) <=10) {
-                System.out.println("Aprovado");
+        } while (nota2 < 0 || nota2 > 10);
+
+        do {
+            System.out.println("nota 3");
+            nota3 = sc.nextInt();
+            if (nota3 < 0 || nota3 > 10) {
+                System.out.println("Nota inválida. Ingrese una nota entre 0 y 10.");
+            }
+        } while (nota3 < 0 || nota3 > 10);
+
+
+            if (((nota1 + nota2 + nota3) / 3) > 0 && ((nota1 + nota2 + nota3) / 3) <= 3)  {
+                resultado ="No aprovado";
+            } else if (((nota1 + nota2 + nota3) / 3) > 3 && ((nota1 + nota2 + nota3) / 3) <= 5) {
+                resultado = "Insuficiente";
+
+            } else if (((nota1 + nota2 + nota3) / 3) > 5 && ((nota1 + nota2 + nota3) / 3) <= 8) {
+                resultado = "Aceptable";
+
+            } else {
+                resultado = "Aprovado";
 
             }
-            else {
-                System.out.println("La nota ingresada es la incorrecta");
-            }
-
+            System.out.println(resultado);
     }
 }
